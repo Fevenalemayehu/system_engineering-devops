eb75cab647a22de8a550dbc982f0a16cdcb2cda4
Feven237 <fevenwolde54@gmail.com>
2024-09-15 22:42:16 +0400

    I created a file

---
+++ Desktop/alx_be_python/python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 5
+sum = number1 + number2
+print("Addition of",number1, "and", number2, "is",sum)
+sub = number1 - number2
+print ("Subtractionof",number1, "and", number2, "is", sub)
+multiplication =number1 * number2
+print ("Multiplication of",number1, "and", number2, "is", multiplication)
